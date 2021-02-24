c7c1b76c6b8bf5ed4ab3341a28acf30bc20daece
stevey-sy <sinsy89@hanmail.net>
2021-02-24 18:59:21 +0900

    [FIX] 알람 기능 / 알람 종료후 앱 정지되는 오류 개선

--- app/src/main/java/com/example/lets_walk_butler/RingtonePlayingService.java
+++ app/src/main/java/com/example/lets_walk_butler/RingtonePlayingService.java
@@ -7,7 +7,6 @@ import android.app.PendingIntent;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
-import android.graphics.Color;
 import android.media.MediaPlayer;
 import android.os.Build;
 import android.os.IBinder;
@@ -15,6 +14,7 @@ import android.os.IBinder;
 import androidx.annotation.Nullable;
 import androidx.annotation.RequiresApi;
 import androidx.core.app.NotificationCompat;
+import androidx.core.app.NotificationManagerCompat;
 
 public class RingtonePlayingService extends Service {
 
@@ -42,20 +42,31 @@ public class RingtonePlayingService extends Service {
             // 알람시간 때에 사용자에게 notification을 보낸다.
             // notification을 클릭할 시, 앱의 메인화면으로 이동
             Intent notificationIntent = new Intent(getApplicationContext(), MainActivity.class);
-            notificationIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK | Intent.FLAG_ACTIVITY_CLEAR_TASK);
+            notificationIntent.setFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK);
             PendingIntent pendingIntent = PendingIntent.getActivity(this, 0, notificationIntent,  PendingIntent.FLAG_UPDATE_CURRENT);
 
-            Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
-                    .setSmallIcon(R.mipmap.ic_launcher)
+//            Notification notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+//                    .setSmallIcon(R.mipmap.ic_launcher)
+//                    .setContentTitle("\"집사야, 산책갈 시간이야!\"")
+//                    .setContentText("산책 시간을 알려드립니다.")
+//                    .setColor(Color.GREEN)
+////                    .setContentIntent(pendingIntent)
+//                    .setAutoCancel(true)
+//                    .setDefaults(Notification.DEFAULT_ALL)
+//                    .setDefaults(Notification.FLAG_AUTO_CANCEL)
+//                    .build();
+//            startForeground(1, notification);
+
+            NotificationCompat.Builder notification = new NotificationCompat.Builder(this, CHANNEL_ID)
+                    .setSmallIcon(R.drawable.ic_paw)
                     .setContentTitle("\"집사야, 산책갈 시간이야!\"")
-                    .setContentText("산책 시간을 알려드립니다.")
-                    .setColor(Color.GREEN)
-                    .setContentIntent(pendingIntent)
-                    .setAutoCancel(true)
                     .setDefaults(Notification.DEFAULT_ALL)
-                    .setDefaults(Notification.FLAG_AUTO_CANCEL)
-                    .build();
-            startForeground(1, notification);
+                    .setContentIntent(pendingIntent)
+                    .setAutoCancel(true);
+            NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+
+            // notificationId is a unique int for each notification that you must define
+            notificationManager.notify(1, notification.build());
         }
     }
 
@@ -79,8 +90,8 @@ public class RingtonePlayingService extends Service {
 
         // 알람음 재생 X , 알람음 시작 클릭
         if(!this.isRunning && startId == 1) {
-            mediaPlayer = MediaPlayer.create(this, R.raw.morning_alarm);
-            mediaPlayer.start();
+//            mediaPlayer = MediaPlayer.create(this, R.raw.morning_alarm);
+//            mediaPlayer.start();
             this.isRunning = true;
             this.startId = 0;
         }
